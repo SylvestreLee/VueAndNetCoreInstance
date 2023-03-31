f3daf844b232b77127ceb85902737b23be29c5b5
ONLY\Prince <vanxingfu@qq.com>
2023-03-31 13:55:37 +0800

    联系程序和SQL提交

---
+++ .Net Core Console/ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,21 @@
+﻿using System.Text;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            string fileName = "D:\\公司培训\\自学\\.Net Core Console\\1.txt";
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < 10000; i++)
+            {
+                sb.AppendLine("Hello World!!!");
+            }
+            await File.WriteAllTextAsync(fileName, sb.ToString());
+            string res = await File.ReadAllTextAsync(fileName);
+            Console.WriteLine(res);
+            Console.ReadLine();
+        }
+    }
+}
